93886e00bad381eb07b28fb04ce96c3fc6c9a2e2
Pal M Sivertsen <palmarius@hotmail.com>
2014-12-22 22:34:42 +0100

    Configured Hibernate

--- src/main/java/no/nith/sivpal12/futurama/quotes/spring/config/SpringMvcConfiguration.java
+++ src/main/java/no/nith/sivpal12/futurama/quotes/spring/config/SpringMvcConfiguration.java
@@ -2,17 +2,16 @@ package no.nith.sivpal12.futurama.quotes.spring.config;
 
 import java.util.Properties;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AvailableSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -29,7 +28,6 @@ import org.springframework.web.servlet.view.JstlView;
         "no.nith.sivpal12.futurama.quotes.repositories"
 })
 @EnableTransactionManagement
-@PropertySource("classpath:application.properties")
 public class SpringMvcConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
@@ -47,57 +45,47 @@ public class SpringMvcConfiguration extends WebMvcConfigurerAdapter {
         return viewResolver;
     }
 
-    private static final String PROPERTY_NAME_DATABASE_DRIVER = "db.driver";
-    private static final String PROPERTY_NAME_DATABASE_PASSWORD = "db.password";
-    private static final String PROPERTY_NAME_DATABASE_URL = "db.url";
-    private static final String PROPERTY_NAME_DATABASE_USERNAME = "db.username";
-
-    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
-    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
-    private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
-
-    @Resource
-    private Environment env;
-
     @Bean
+    public SessionFactory sessionFactory() {
+        LocalSessionFactoryBuilder builder =
+                new LocalSessionFactoryBuilder(dataSource());
+        builder.scanPackages("no.nith.sivpal12.futurama.quotes.entities")
+                .addProperties(getHibernateProperties());
+
+        return builder.buildSessionFactory();
+    }
+
+    private Properties getHibernateProperties() {
+        // https://devcenter.heroku.com/articles/heroku-postgresql#spring-java
+        // URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        // String username = dbUri.getUserInfo().split(":")[0];
+        // String password = dbUri.getUserInfo().split(":")[1];
+        // String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':'
+        // + dbUri.getPort() + dbUri.getPath();
+
+
+        Properties prop = new Properties();
+        prop.put(AvailableSettings.FORMAT_SQL, "true");
+        prop.put(AvailableSettings.SHOW_SQL, "true");
+        prop.put(AvailableSettings.DIALECT,
+                "org.hibernate.dialect.PostgreSQL9Dialect");
+        return prop;
+    }
+
+    @Bean(name = "dataSource")
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
-        dataSource.setDriverClassName(env
-                .getRequiredProperty(PROPERTY_NAME_DATABASE_DRIVER));
-        dataSource.setUrl(env.getRequiredProperty(PROPERTY_NAME_DATABASE_URL));
-        dataSource.setUsername(env
-                .getRequiredProperty(PROPERTY_NAME_DATABASE_USERNAME));
-        dataSource.setPassword(env
-                .getRequiredProperty(PROPERTY_NAME_DATABASE_PASSWORD));
-
-        return dataSource;
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("org.postgresql.Driver");
+        ds.setUrl("jdbc:postgresql://localhost:5432/futurama-quotes");
+        ds.setUsername("futurama-app");
+        ds.setPassword("postgres");
+        return ds;
     }
 
+    // Create a transaction manager
     @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-        sessionFactoryBean.setDataSource(dataSource());
-        sessionFactoryBean
-                .setPackagesToScan(env
-                        .getRequiredProperty(PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN));
-        sessionFactoryBean.setHibernateProperties(hibProperties());
-        return sessionFactoryBean;
+    public HibernateTransactionManager txManager() {
+        return new HibernateTransactionManager(sessionFactory());
     }
 
-    private Properties hibProperties() {
-        Properties properties = new Properties();
-        properties.put(PROPERTY_NAME_HIBERNATE_DIALECT,
-                env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
-        properties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL,
-                env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
-        return properties;
-    }
-
-    @Bean
-    public HibernateTransactionManager transactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());
-        return transactionManager;
-    }
 }
