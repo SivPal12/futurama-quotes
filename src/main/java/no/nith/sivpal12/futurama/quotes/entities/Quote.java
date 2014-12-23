b4077c41e3715ff8bc95a11a1aa1d2ccaabc919b
Pal M Sivertsen <palmarius@hotmail.com>
2014-12-23 20:46:42 +0100

    Fixed id generation of quote

--- src/main/java/no/nith/sivpal12/futurama/quotes/entities/Quote.java
+++ src/main/java/no/nith/sivpal12/futurama/quotes/entities/Quote.java
@@ -1,15 +1,14 @@
 package no.nith.sivpal12.futurama.quotes.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.hibernate.annotations.Generated;
-import org.hibernate.annotations.GenerationTime;
 
 @Entity
 public class Quote {
     @Id
-    @Generated(GenerationTime.INSERT)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String quote;
 
